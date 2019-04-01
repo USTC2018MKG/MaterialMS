c7629fdc0255c8a846119f7055cc88209403bef1
gaoran <2455156635@qq.com>
2019-04-01 21:13:44 +0800

    add page

--- MaterialMS/MainWindow.xaml.cs
+++ MaterialMS/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace MaterialMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        UserManagePage userManagePage;
+        MaterialManagePage materialManagePage = new MaterialManagePage();
+        OrderDetailPage orderDetailPage;
+        OrderFormPage orderFormPage;
         public MainWindow()
         {
             InitializeComponent();
+            UserManagePage userManagePage = new UserManagePage();
+            gridPage.Navigate(userManagePage);
+        }
+
+        private void userPageClick(object sender, RoutedEventArgs e)
+        {
+            gridPage.Navigate(userManagePage);
+         
+        }
+
+        private void materialClick(object sender, RoutedEventArgs e)
+        {
+            gridPage.Navigate(materialManagePage);
+            //gridPage.Children.Add(new MaterialManagePage());
+        }
+
+        private void orderPageClick(object sender, RoutedEventArgs e)
+        {
+            //gridPage.Children.Add(new OrderFormPage());
+        }
+
+        private void orderDetPageClick(object sender, RoutedEventArgs e)
+        {
+           // gridPage.Children.Add(new OrderDetailPage());
         }
     }
 }
---
+++ MaterialMS/OrderFormPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialMS
+{
+    /// <summary>
+    /// OrderFormPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class OrderFormPage : Page
+    {
+        public OrderFormPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
