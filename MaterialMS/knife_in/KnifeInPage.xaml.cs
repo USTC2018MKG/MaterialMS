2a326516e3882bcb8d93def307eea2a9965c7def
gaoran <2455156635@qq.com>
2019-05-26 17:51:26 +0800

    入库更新数据库

--- MaterialMS/knife_in/KnifeInPage.xaml.cs
+++ MaterialMS/knife_in/KnifeInPage.xaml.cs
@@ -25,13 +25,12 @@ namespace MaterialMS.knife_in
     {
         private MySqlConnection conn = new MySqlConnection(Constant.myConnectionString);
         private Material material;
-        private List<Material> materials;
+        private ObservableCollection<Material> materials = new ObservableCollection<Material>();
         private ObservableCollection<Material> os;
 
         public KnifeInPage()
         {
             InitializeComponent();
-            materials = new List<Material>();
         }
 
         private void Search_Click(object sender, RoutedEventArgs e)
@@ -56,22 +55,26 @@ namespace MaterialMS.knife_in
                     cmd.Parameters["@mid"].Value = txtMid.Text;
                     MySqlDataReader sdr = cmd.ExecuteReader();
                     Console.WriteLine(sdr);
-                    if (sdr.Read()) {
-                        os = new ObservableCollection<Material>() {
-                          new Material(){
-                              mid=sdr["mid"].ToString(),
-                              rest=sdr["rest"].ToString(),
-                              mname=sdr["mname"].ToString(),
-                              add_num="",
-                              buy_type=sdr["buy_type"].ToString(),
-                              cycle = sdr["cycle"].ToString(),
-                              each_price = sdr["each_price"].ToString(),
-                              repository_id = sdr["repository_id"].ToString()
-                          }
-
-                        };
-                        Console.WriteLine(sdr["mname"].ToString());
+                    if (!sdr.Read()) {
+                        tblSearchMsg.Content = "刀具不存在！请重新输入";
+                        txtMid.Text = "";
+                        txtMid.Focus();
                     }
+                    else{
+                        os = new ObservableCollection<Material>();
+                        Material material = new Material();
+                        material.mid = sdr["mid"].ToString();
+                        material.rest = sdr["rest"].ToString();
+                        material.mname = sdr["mname"].ToString();
+                        material.add_num = "";
+                        material.buy_type = sdr["buy_type"].ToString();
+                        material.cycle = sdr["cycle"].ToString();
+                        material.each_price = sdr["each_price"].ToString();
+                        material.repository_id = sdr["repository_id"].ToString();                            
+                        Console.WriteLine(sdr["mname"].ToString());
+                        os.Add(material);
+                    }
+                    
                     knife.ItemsSource = os;
                 }
                 catch (MySqlException ex)
@@ -87,44 +90,96 @@ namespace MaterialMS.knife_in
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = sender as Button;
-            knife.SelectedItem = btn.DataContext;
-            DataRowView rowSelected = knife.SelectedItem as DataRowView;
-            
-            
-            material = new Material();
-            if (rowSelected != null)
-            {  
-                material.rest = rowSelected["rest"].ToString();
-                material.mid = rowSelected["mid"].ToString();               
-                String tbText = btn.Tag as String;
-                material.add_num = tbText;
-            }
-            string addnum = material.add_num;
-            string msg = "确定要入库刀具" + material.mid + ":" + os[0].add_num + "只吗?";
-            MessageBoxResult dr = MessageBox.Show(msg, "刀具入库", MessageBoxButton.OKCancel, MessageBoxImage.Question);
-            if (dr == MessageBoxResult.OK)
+            if (isNum(os[0].add_num))
             {
-                material.add_num = addnum;
-                materials.Add(material);
-                MessageBox.Show("添加成功!");
+                string addnum = os[0].add_num;
+                string msg = "确定要入库刀具" + os[0].mid + ":" + os[0].add_num + "只吗?";
+                MessageBoxResult dr = MessageBox.Show(msg, "刀具入库", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                if (dr == MessageBoxResult.OK)
+                {                    
+                    materials.Add(os[0]);
+                    MessageBox.Show("添加成功!");
+                    knifeAddInfo.ItemsSource = materials;
+                }
             }
-            foreach(Material m in materials)
-            {
-                Console.WriteLine(m.mid);
+            else {
+                MessageBox.Show("入库数量有误");
             }
+            
         }
 
         private void InList_Click(object sender, RoutedEventArgs e)
         {
+            //连接数据库对象
+            MySqlConnection conn = new MySqlConnection(Constant.myConnectionString);
+            conn.Open();
+            //打开通道，建立连接，可能出现异常,使用try catch语句   
+            MySqlTransaction transaction = conn.BeginTransaction();
+            string in_id = System.Guid.NewGuid().ToString("N");
+            string sql = string.Format("insert into in_order (in_id,in_time,employee_id) values('{0}','{1}','{2}')",
+                in_id, DateTime.Now.ToString("yyyy-MM-dd"), Account.Instance.GetUser().name);
 
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn, transaction);
+                int result = cmd.ExecuteNonQuery();
+                if (result != 0)
+                {
+                    int count = 0;
+                    foreach (Material item in materials)
+                    {
+                        int restNew = int.Parse(item.rest) + int.Parse(item.add_num);
+                        string msql = string.Format("update material set rest='{0}' where mid='{1}'", restNew, item.mid);
+                        MySqlCommand mycmd1 = new MySqlCommand(msql, conn, transaction);
+                        mycmd1.ExecuteNonQuery();
+                        string insql = string.Format("insert into in_item (in_id,mid,num) values('{0}','{1}','{2}')",
+                            in_id,item.mid,int.Parse(item.add_num));
+                        MySqlCommand mycmd2 = new MySqlCommand(insql, conn, transaction);
+                        mycmd2.ExecuteNonQuery();
+                        count++;
+                    }
+                    if (count == materials.Count)
+                    {
+                        MessageBox.Show("入库成功");
+                        transaction.Commit();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("插入失败!");
+                    transaction.Rollback();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("插入失败!");
+                transaction.Rollback();
+            }
+            finally {
+                conn.Close();
+            }
+            
+        }
+
+        private void ClearList_Click(object sender, RoutedEventArgs e)
+        {
+            materials = new ObservableCollection<Material>();
+            knifeAddInfo.ItemsSource = materials;
+            
         }
 
         private void knifesItemClick(object sender, SelectionChangedEventArgs e)
         {
             //Console.Write(sender);
         }
-        
+
+        private Boolean isNum(String add_num) {
+            int a = 0;
+            if (add_num != null && !add_num.Equals("") && int.TryParse(add_num, out a))           
+                return true;          
+            else
+                return false;
+        }
 
     }
 }
